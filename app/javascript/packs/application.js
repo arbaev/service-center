f5c9382b1a4ac355dc0d52d6d984fa2ad239e0f3
Tim Arbaev <timbrk@gmail.com>
2019-09-08 18:01:11 +0300

    add devise model Staff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("@rails/activestorage").start();
+require("channels");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -14,3 +14,15 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import Vue from 'vue'
+import Staff from '../components/staff.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const app = new Vue({
+        render: h => h(Staff)
+    }).$mount();
+    document.body.appendChild(app.$el);
+
+    console.log(app)
+});
