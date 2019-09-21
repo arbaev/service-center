cef95d28cba199077fc1d4fb76856dd76f737d52
Tim Arbaev <timbrk@gmail.com>
2019-09-21 10:08:02 +0300

    add fullname and phone to clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,8 @@
 
 class Client < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
+
+  validates :email, :fullname, :phone, presence: true
+  validates :fullname, length: { minimum: 5 }
+  validates :phone, numericality: { only_integer: true }
 end
---
+++ spec/models/client_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Client, type: :model do
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of :fullname }
+  it { should validate_presence_of :phone }
+  it { should validate_numericality_of :phone }
+end
