ae65e236558bba60be27407ea912554364da947e
Tim Arbaev <timbrk@gmail.com>
2019-09-26 13:50:13 +0300

    improve Staff/HomeController tests

--- spec/controllers/staff/home_controller_spec.rb
+++ spec/controllers/staff/home_controller_spec.rb
@@ -4,19 +4,37 @@ require 'rails_helper'
 
 RSpec.describe Staff::HomeController, type: :controller do
   let(:staff) { create(:staff) }
+  let(:client) { create(:client) }
 
   describe 'GET #index' do
-    before do
-      login(staff)
-      get :index
+    context 'authorized as Staff' do
+      before do
+        login(staff)
+        get :index
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status :success
+      end
+
+      it 'renders index view' do
+        expect(response).to render_template :index
+      end
     end
 
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
-    end
+    context 'unauthorized' do
+      it 'returns error when not logged' do
+        get :index
 
-    it 'renders index view' do
-      expect(response).to render_template :index
+        expect(response).to have_http_status :found
+      end
+
+      it 'returns error when logged as Client' do
+        login(client)
+        get :index
+
+        expect(response).to have_http_status :found
+      end
     end
   end
 end
