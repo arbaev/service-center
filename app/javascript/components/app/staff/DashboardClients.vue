43c244bb0f13d7351207f4504f241ba99e47bb49
Tim Arbaev <timbrk@gmail.com>
2019-10-04 12:31:51 +0300

    add Spinner for DashboardClients component

--- app/javascript/components/app/staff/DashboardClients.vue
+++ app/javascript/components/app/staff/DashboardClients.vue
@@ -7,22 +7,28 @@
       .col-12.col-sm-6.q-pa-sm
         #clients-list(v-show="clientsList.length")
           h4.text-h4 Clients list
-          ul
+          q-spinner-gears(v-if="clientsListLoading" color="blue-grey-6" size="3em")
+          ul(v-else)
             li(v-for="client in clientsList" :key="client.id" :client="client") {{ client.id }}: {{ client.attributes.email }}
 </template>
 
 <script>
   import NewClientForm from '../staff/NewClientForm'
   import {backendGet} from '../api'
+  import {
+    QSpinnerGears,
+  } from 'quasar'
 
   export default {
     data: function () {
       return {
-        clientsList: []
+        clientsList: [],
+        clientsListLoading: true
       }
     },
     components: {
-      NewClientForm
+      NewClientForm,
+      QSpinnerGears,
     },
     created() {
       this.fetchClientsList();
@@ -31,7 +37,8 @@
       fetchClientsList() {
         backendGet('/staff/client')
           .then(response => this.clientsList = response.data.data)
-          .catch(error => console.log(error));
+          .catch(error => console.log(error))
+          .finally(() => this.clientsListLoading = false);
       },
     }
   }
