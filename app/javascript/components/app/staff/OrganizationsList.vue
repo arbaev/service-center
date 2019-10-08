b4f10d8f9d67049cac50c5e512d6ddbc257531aa
Tim Arbaev <timbrk@gmail.com>
2019-10-08 13:35:09 +0300

    move organization list to component

---
+++ app/javascript/components/app/staff/OrganizationsList.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  #organizations-list
+    h4.text-h4 Organizations list
+    q-table(
+      :data="organizationsList"
+      no-data-label="No organizations yet"
+      :columns="columns"
+      row-key="name")
+</template>
+
+<script>
+  import {
+    QSpinnerGears,
+    QTable,
+  } from 'quasar'
+
+  export default {
+    props: ['organizationsList'],
+    data: function () {
+      return {
+        columns: [
+          {
+            name: 'name',
+            required: true,
+            label: 'Название',
+            align: 'left',
+            field: row => row.attributes.name,
+            format: val => `${val}`,
+            sortable: true
+          },
+          { name: 'id', align: 'center', label: '#id', field: 'id', sortable: true },
+          { name: 'orgType', label: 'Тип', sortable: true, field: row => row.attributes.org_type.name },
+          { name: 'inn', label: 'ИНН', field: row => row.attributes.inn },
+          { name: 'ogrn', label: 'ОГРН', field: row => row.attributes.ogrn },
+        ],
+      }
+    },
+    components: {
+      QSpinnerGears,
+      QTable,
+    },
+  }
+</script>
