770a66cf3ed9fb390cf446b333d230f99760914b
Tim Arbaev <timbrk@gmail.com>
2019-10-09 08:36:36 +0300

    add 'no organizations' slot for QTable

--- app/javascript/components/app/staff/OrganizationsList.vue
+++ app/javascript/components/app/staff/OrganizationsList.vue
@@ -8,12 +8,19 @@
       :loading="loading"
       :pagination.sync="pagination"
       row-key="name")
+
+      template(v-slot:no-data="{ icon, message }")
+        .full-width.row.flex-center.text-accent.q-gutter-sm
+          q-icon(size="2em" name="fas fa-exclamation-triangle")
+          span Well this is sad... {{ message }}
+
 </template>
 
 <script>
   import {
     QSpinnerGears,
     QTable,
+    QIcon,
   } from 'quasar'
 
   export default {
@@ -45,6 +52,7 @@
     components: {
       QSpinnerGears,
       QTable,
+      QIcon,
     },
   }
 </script>
